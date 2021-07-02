295010fb487ce83b828e424cdeadd377ceaa6dff
Ilyas Belaoud <ilyassbelaoud.99@gmail.com>
2021-07-02 13:35:49 +0100

    new version

---
+++ popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var checkPageButton = document.getElementById('clickIt');
+    checkPageButton.addEventListener('click', function () {
+
+        window.open("options.html", "_blank");
+    }, false);
+}, false);
